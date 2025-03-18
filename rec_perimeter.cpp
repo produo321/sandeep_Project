7b72ad3d2f041a49d4a36dbc43c05b4f4699f2f1
produo <produo321@gmail.com>
2025-03-18 21:37:10 +0530

    addition

---
+++ rec_perimeter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float l,b,area,perimeter;
+    cout<< "enter length and bregth of rectangle";
+    cin>> l>> b;
+    area = l*b;
+    perimeter = 2*(l+b);
+    cout << " area of rectangle"<< area;
+    cout << " \n perimeter of rectangle" <<perimeter;
+    return 0;
+}
---
+++ result.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float i,j,result;
+    cout<<"enter number";
+    cin>> i>>j;
+    result= i*j;
+
+    cout<<"result"<<result;
+    return 0;
+}
