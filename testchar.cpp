95176327a63f5effba721ec5909234e4f936fc2c
produo <produo321@gmail.com>
2025-03-18 21:57:45 +0530

    testchar

---
+++ testchar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int x;
+    cout<<"\n enter the character";
+    cin>>ch;
+    x=ch;
+    cout<<"\n Character value is :-" <<ch;
+    cout<<"\n Ascii value is :"<<x;
+    return 0;
+}
