42980718a4271d31550e242688f1a2988ab72e0d
produo <produo321@gmail.com>
2025-03-19 13:03:56 +0530

    areaperiRec

---
+++ compoundinterest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double principal = 10000, rate = 5, time = 2;
+    // calculate compound interest
+    double A = principal * ((pow((1 + rate / 100),time)));
+    double  CI = A - principal;
+    cout << "Compound interest is :- "<< CI;
+    return 0;
+    
+}
--- rec_perimeter.cpp
+++ rec_perimeter.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     float l,b,area,perimeter;
-    cout<< "enter length and bregth of rectangle";
+    cout<< "enter length and bregth of rectangle   :-  ";
     cin>> l>> b;
     area = l*b;
     perimeter = 2*(l+b);
-    cout << " area of rectangle"<< area;
-    cout << " \n perimeter of rectangle" <<perimeter;
+    cout << " area of rectangle   :  "<< area;
+    cout << " \n perimeter of rectangle   :"   <<perimeter;
     return 0;
 }
